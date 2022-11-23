adce1bf3055951ee686a0f7f023c0a6473fa20b7
Stas Kobles <sskobles@gmail.com>
2022-11-23 19:10:46 +0400

    ROUTING

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
 import "./App.css";
+import AppRouter from "./components/AppRouter";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="app">
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <AppRouter />
+    </BrowserRouter>
   );
 }
 
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,21 +1,26 @@
 import React from "react";
-import { createBrowserRouter, Link } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { CHAT_ROUTE, LOGIN_ROUTE } from "../utils/consts";
 import Chat from "./Chat";
 import Login from "./Login";
 
-const user = false;
-const router = user
-  ? createBrowserRouter([
-      {
-        path: "chat",
-        element: Chat,
-      },
-    ])
-  : createBrowserRouter([
-      {
-        path: "login",
-        element: Login,
-      },
-    ]);
+const AppRouter = () => {
+  const user = false;
+  return (
+    <Routes>
+      {user ? (
+        <>
+          <Route path={CHAT_ROUTE} element={<Chat />} />
+          <Route path="*" element={<Navigate to={CHAT_ROUTE} />} />
+        </>
+      ) : (
+        <>
+          <Route path={LOGIN_ROUTE} element={<Login />} />
+          <Route path="*" element={<Navigate to={LOGIN_ROUTE} />} />
+        </>
+      )}
+    </Routes>
+  );
+};
 
-export default router;
+export default AppRouter;
